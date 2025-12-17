a12e4c2fad5552ff4aa72b751207b7f6e7624b85
Abdirahman Abshir <abdirahmanabshir100@gmail.com>
2025-12-17 23:49:58 +0300

    sh

--- cloud_week2/futuro.sh
+++ cloud_week2/futuro.sh
@@ -2,3 +2,4 @@
 
 echo "Hey Abdi you are the best Cloud Engineer i have ever seen"
 echo "Learning all this programming is challenging but it'll be worth it."
+echo "Keep pushing forward, and don't forget to take breaks!"
